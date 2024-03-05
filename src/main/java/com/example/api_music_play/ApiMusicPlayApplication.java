c89179f0c60190c809c09f693dc8422f5fbbc180
hieulag15 <hieuthanhtran12@gmail.com>
2024-03-05 15:06:27 +0700

    init API

---
+++ src/main/java/com/example/api_music_play/ApiMusicPlayApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_music_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMusicPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMusicPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_music_play/ApiMusicPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_music_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMusicPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
