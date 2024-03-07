104c665b808710fc7f22de813edeb12e5f4d4b7d
hieulag15 <hieuthanhtran12@gmail.com>
2024-03-07 13:57:24 +0700

    update and delete CategoryController

--- src/main/java/com/example/api_music_play/Controller/CategoryController.java
+++ src/main/java/com/example/api_music_play/Controller/CategoryController.java
@@ -1,14 +1,14 @@
 package com.example.api_music_play.Controller;
 
 import com.cloudinary.Cloudinary;
+import com.example.api_music_play.Exception.RourceNotFoundException;
 import com.example.api_music_play.Mapper.CategoryMapper;
+import com.example.api_music_play.Model.Category;
 import com.example.api_music_play.ModelDTO.CategoryDTO;
 import com.example.api_music_play.ModelMessage.CategoryMessage;
 import com.example.api_music_play.Repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +32,32 @@ public class CategoryController {
     @PostMapping(value = "/all")
     public List<CategoryDTO> getAllCategory(){
         return categoryMapper.getListCategory(categoryRepository.findAll());
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public CategoryMessage update(@PathVariable long id, @RequestBody Category category){
+        CategoryMessage categoryMessage = new CategoryMessage();
+        Category categoryUpdate = categoryRepository.findById(id).orElseThrow(()-> new RourceNotFoundException("Category not exist with id: " + id));
+        categoryUpdate.setName(category.getName());
+        categoryUpdate.setDescription(category.getDescription());
+        categoryRepository.save(categoryUpdate);
+        categoryMessage.setCategory(categoryUpdate);
+        categoryMessage.setMessage("Successfull");
+        return categoryMessage;
+    }
+
+    @PostMapping(value = "/delete")
+    public CategoryMessage delete(@RequestParam Long id){
+        CategoryMessage categoryMessage = new CategoryMessage();
+        Category category = categoryRepository.findById(id).orElseThrow(()-> new RourceNotFoundException("Category not exist with id: " + id));
+        if (category != null) {
+            categoryRepository.delete(category);
+            categoryMessage.setMessage("Successfull");
+            return categoryMessage;
+        } else {
+            categoryMessage.setMessage("Failde");
+            return categoryMessage;
+        }
+
     }
 }
