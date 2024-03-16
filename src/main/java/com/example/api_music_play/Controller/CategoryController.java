667f4d70edbddbebc026ba128c3ddfd315c03b21
hieulag15 <hieuthanhtran12@gmail.com>
2024-03-16 22:01:12 +0700

    createCategory

--- src/main/java/com/example/api_music_play/Controller/CategoryController.java
+++ src/main/java/com/example/api_music_play/Controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.example.api_music_play.Controller;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.example.api_music_play.Exception.RourceNotFoundException;
 import com.example.api_music_play.Mapper.CategoryMapper;
 import com.example.api_music_play.Model.Category;
@@ -9,8 +10,11 @@ import com.example.api_music_play.ModelMessage.CategoryMessage;
 import com.example.api_music_play.Repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -59,5 +63,34 @@ public class CategoryController {
             return categoryMessage;
         }
 
+    }
+
+    @PostMapping(value = "/create")
+    public CategoryMessage createCategory(@RequestParam String name, @RequestParam("image")MultipartFile image, @RequestParam String description) throws IOException {
+        CategoryMessage categoryMessage = new CategoryMessage();
+        Category category = new Category();
+        String linkImage = "";
+
+        try {
+            Map<String, Object> uploadResult = cloudinary.uploader().upload(image.getBytes(), ObjectUtils.asMap("resource_type", "auto"));
+            linkImage = uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        category.setName(name);
+        category.setImage(linkImage);
+        category.setDescription(description);
+
+        try {
+            categoryRepository.save(category);
+            categoryMessage.setCategory(category);
+            categoryMessage.setMessage("You have successfull created a Category");
+            return categoryMessage;
+        } catch (Exception e) {
+            categoryMessage.setCategory(null);
+            categoryMessage.setMessage("You have failed to create a Category");
+            return categoryMessage;
+        }
     }
 }
