c03cbce3ecbba469ff343be63c6e10a3fbaa8e0a
hieulag15 <hieuthanhtran12@gmail.com>
2024-03-28 00:28:42 +0700

    fix database

--- src/main/java/com/example/api_music_play/ApiMusicPlayApplication.java
+++ src/main/java/com/example/api_music_play/ApiMusicPlayApplication.java
@@ -1,5 +1,7 @@
 package com.example.api_music_play;
 
+import com.example.api_music_play.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
